57274166f41fa32d2ea10b1cff1fdb6b7c8e9291
madan0698 <madan.hyr25@gmail.com>
2026-02-16 23:37:42 +0530

    set.py

---
+++ set.py
@@ -0,0 +1,67 @@
+# set = {1, 2, 3,4 }
+
+# print(type(set))   
+
+# set1 = set([1,2,3])
+
+# print(type(set1))  
+
+# set1 = set("1,2,3")
+
+# print(set1)            #it will print it in unordered
+
+# set1 = {1,2,3,5,3,2,9}
+
+# print(set1)              #it will not print duplicate elements, it will remove it
+
+#difference
+
+set1 = {'A', 'B', 'C', 'D', 'E'}
+
+set2 = {'J', 'B', 'K', 'D', 'L'}
+
+# set = set1 - set2           #it will print elements present in set1 not in set2
+
+# set = set2 - set1           #it will print elements present  in set2 not in set1
+
+# print(set)  
+
+# print(set1.difference(set2))
+
+# UNION
+
+# set = set1 | set2              # OR operator is used to print union elements
+
+# set1.update(set2)
+
+# print(set)
+
+# # set1 = set1 | set2
+
+# print(f"set1 ==> {set1}")
+# print(f"set2 ==> {set2}")
+
+#intersection
+
+# set = set1 & set2            #and operator is used to print common elements present in set1 and set2
+
+# print(set)
+
+#set = set1.intersection(set2)
+
+# set = set1.intersection_update(set2)
+
+# print(set)
+
+# to remove random elements 
+
+# set1.pop()
+
+# print(set1)
+
+# to remove particular element 
+
+# set1.remove("A") 
+
+# print(set1)
+
