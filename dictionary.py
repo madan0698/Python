f2f0a75ab7ee1af21e8cb90800281c8033e5c799
madan0698 <madan.hyr25@gmail.com>
2026-02-17 00:11:37 +0530

    dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,31 @@
+# x = (10, 20)
+
+# print(hash(x))    #in this way we can check hashable or not
+
+# # #syntax
+# # data= {
+# #     key:value
+# # }
+
+
+# student_data= {
+#     "Name": "Madan",
+#     "Name": "Abhi"
+# }
+
+# print(student_data)         #it will print last updated value, Dictionary is also ordered
+
+student_data= {
+    "Name": "Madan",
+    "Age": 27, 
+    "Skills": ["cricket","vollyball"],
+    "Parents Name":{
+        "fathername": "Nagendrappa",
+        "mothername": "Renuka",
+    }
+}
+
+print(student_data) 
+print(type(student_data))
+
+
