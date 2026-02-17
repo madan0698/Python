0c5d60ef9b9fdf1b28f9bc13f71afc7aa839f349
madan0698 <madan.hyr25@gmail.com>
2026-02-17 20:29:32 +0530

    operators.py

---
+++ operators.py
@@ -0,0 +1,108 @@
+# Assignment
+#a = 10
+
+# a += 1     # a = a + 1
+
+# print(a)
+
+# set1 = {'A', 'B', 'C', 'D', 'E'}
+# set2 = {'P', 'B', 'Q', 'R', 'E', 'W'}
+
+# set1 |= set2 
+
+# set1 = set1 | set2
+
+# print(set1)
+
+# Arthmatic 
+# + - addition , - - substraction, / - Division, * -Multiplication,
+#  ** -exponentiation , % - modulous, // - 
+
+# a = 3
+
+# out = a ** 3
+
+# print(out)
+
+# equal
+# gt
+# gte
+# lt
+# tte
+# ne
+
+# Comparison Operators
+# a = 13
+# b = 12
+# print(a == b)
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+# print(a != b)
+
+
+
+
+# Logical Operators
+# and
+# or
+# not
+
+# a = 10
+# b =10
+
+# print(a>9 and b>9)           #true
+
+# a = 10
+# b =10
+
+# print(a>9 or b<9)            #true
+
+# a = False
+# b = True
+
+# print(a and b)                 #false
+# print(not (a and b))           #true
+
+#  Membership Operator (in operator)
+# states = ['KA', 'KE', 'AP', 'TN', 'TE']
+
+# print('AS'in states)             #False
+# print('AS' not in states)        #True
+# print('TN' not in states)        #False
+
+
+#Identity Operator  (is operator)
+
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+# c = a
+
+
+# print(a is b)
+# print(a == b)
+
+# print(a is not c)
+# # print(a == c)
+
+# print(id(a))
+# print(id(b))
+# print(id(c))
+
+#Bitwise Operator (not for devops engineers)
+
+# a = 10
+# b = 3
+
+# print(a & b)
+# print(a | b)
+# print(a ^ b)
+
+# print(a << 1)
+# print(a >> 1)
+
+
+# Membership Operator
+# states = ['KA', 'KE', 'AP', 'TN', 'TE']
+# print('TN' not in states)
