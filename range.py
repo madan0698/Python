364375aec9cbc7348c267e7b4e562f47f8e1e08d
madan0698 <madan.hyr25@gmail.com>
2026-02-17 15:00:57 +0530

    range.py

---
+++ range.py
@@ -0,0 +1,13 @@
+# rng1 = range(10)
+
+# print(rng1)
+
+# print(list(rng1))     #it will display 0 to 9 elements
+
+# rng1 = range(1,11)
+
+# print(list(rng1))       #it will print in revers order
+
+rng1 = range(-10, 5)
+
+print(list(rng1)) 
