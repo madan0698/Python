baa9cef242796e36dd53dbfdab8f6e85f9476204
madan0698 <madan.hyr25@gmail.com>
2026-02-19 13:02:50 +0530

    conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,103 @@
+# a = True
+
+# if a:
+#     print("a is true we can execute")
+
+# print("execution can be done")
+
+# a = False
+
+# if a:
+#     print("a is true we can execute")
+
+# print("execution can be done")
+
+# a = 0                                        #falsy value
+# if a:
+#     print("a is true we can execute")
+
+# print("execution can be done")
+
+# a = 0
+
+# if a:
+#     print("a is true")
+#     print("we can execute")
+
+# print("at any time")
+# print("execution can be done")
+
+
+# a = True
+# b = False
+
+# if a and b:
+#     print("a is true")
+#     print("we can execute")
+
+# print("execution can be done")
+
+
+# num1= 1000
+# num2= 500
+
+# if num1 > num2:                                            
+#     print(f"num1({num1}) is greater than num2({num2})")
+# else:
+#     print(f"num1({num1}) is lesser than num1({num2})") 
+
+# print("execution can be done")
+
+# num1 = 15
+# num2 = 13
+# num3 = 14
+
+# if num1 > num2 and num1 > num3:
+#     print("num1 is greater")
+
+# elif num2 > num3 and num2 > num3:
+#     print("num2 is greater")
+
+# else:
+#     print("num3 is greater")
+
+# print("execution can be done")
+
+# s
+
+# num1 = 15
+# num2 = 13
+# num3 = 14
+
+# if num1 > num2 and num1 > num3:
+#     print("num1 is greater")
+
+# elif num2 > num3 and num2 > num3:
+#     print("num2 is greater")
+
+# elif num3 > num1 and num3 > num2:
+#     print("num3 is greater")
+
+# else:
+#     print("all are equal")
+
+# print("execution can be done")
+
+
+# num1 = 15
+# num2 = 13
+# num3 = 15
+
+# if num1 == num2:
+#     print("num1 and num2 areequal")
+# else:
+#     print("num1 and num2 are not equal")
+
+# if num1 == num3:
+#     print("num1 and num3 are equal")
+# else:
+#     print("num1 and num3 are not equal")
+
+# Nested if statement
+
+if 
