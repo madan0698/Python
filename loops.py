d9f20d01ad9a68079d97aee4851b24002ec7ce19
madan0698 <madan.hyr25@gmail.com>
2026-02-23 22:53:07 +0530

    loops.py

---
+++ loops.py
@@ -0,0 +1,76 @@
+# For loop
+
+# for me in "India":
+#     print(me)
+
+
+# for me in "India":
+#      print(f"the element is {me}")
+
+# for me in range(10):
+#      print(f"the element is {me}")
+
+# states = ("KA", "KE", "AP", "TN", "TL")
+
+# for el in states:
+#     print(f"the south indian states are {el}")
+
+# counter = 5
+# for el in range(3):
+#     counter += 10
+#     print(counter)
+
+# print(counter)
+
+# j = 200 
+
+# for abcd in [2,3,4]:
+#     #  print(abcd)
+#      print(j) 
+# print (j)
+
+weeks = ["monday", "tuesday", "saturday", "friday", "wednesday", "thursday", "sunday"]
+
+
+# for week in weeks:
+#     if week != "saturday" and week != "sunday":
+#         print(f"the day is {week}, its weekday")
+#     else:
+#         print(f"the day is {week}, its weekend")
+
+
+# for week in weeks:
+   
+#     if week == "saturday" or week == "sunday":
+#          print(f"the day is {week}, its weekend")
+
+
+# using continue statement
+    
+# for week in weeks:
+
+#     if week == "tuesday":
+#         continue                       # continue keyword
+   
+#     if week == "saturday" or week == "sunday":
+#         print(f"the day is {week}, its weekend")
+#     else:
+#         print(f"the day is {week}, its weekday")
+
+
+
+# using break statement
+    
+for week in weeks:
+
+    print (week)
+    
+    print("ok1")
+    break                      # break keyword
+
+print("execution completed")
+
+    
+
+
+
