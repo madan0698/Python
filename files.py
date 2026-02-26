c9be838f87e23f5878f134e80fdffe8562380db3
madan0698 <madan.hyr25@gmail.com>
2026-02-26 21:48:03 +0530

    file1.txt

--- files.py
+++ files.py
@@ -1,3 +1,12 @@
-f1 = open ('file1.txt', 'a')
+# file1 = open('file1.txt', 'w')
+file1 = open('file1.txt', 'a+')
 
-f1.write("\ngood morning\n")
+file1.seek(0)
+
+# print(file1.readline())
+# print(file1.readline())
+
+# for line in (file1.readlines()):
+#     print(line)
+
+file1.close()
